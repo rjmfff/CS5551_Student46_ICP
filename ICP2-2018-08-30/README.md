ad74b596d96c9ab75aaa13c5bd560c15e739877e
Rachel "Wil Sha" Morris <racheljmorris@gmail.com>
2018-09-01 14:58:50 -0500

    Updated ICP2 to use angular

--- ICP2-2018-08-30/README.md
+++ ICP2-2018-08-30/README.md
@@ -5,3 +5,12 @@ Note: The instructions **do not mention** that AngularJS is a required part of t
 ## Tutorial
 
 I wrote up a tutorial for ICP 2 here: https://github.com/rjmfff/CS5551_Student46_ICP/wiki/ICP-%232-tutorial
+
+This doesn't include the AngularJS aspect, since I didn't use AngularJS originally because the
+specification didn't *mention* AngularJS, so...
+
+## AngularJS Tutorials
+
+* [w3schools](https://www.w3schools.com/angular/angular_intro.asp)
+
+
--- ICP2-2018-08-30/icp2.js
+++ ICP2-2018-08-30/icp2.js
@@ -1,35 +1,41 @@
 /* Rachel Singh */
 
-$( document ).ready( function() {
-
-    apiStringBase = "http://api.wunderground.com/api/4bbbc25f4f5946dd/hourly/q/"; //state%20code/cityname.json";
+var app = angular.module( "WeatherApp", [] );
+app.controller( "WeatherController", function( $scope ) {
+    $scope.weather = [ { time: "a", condition: "b", temperature: "c", feelslike: "d"} ];
     
-    $( "#weatherButton" ).click( function() {
-        FindWeather( $("#city").val(), $("#state").val() );
-    } );
+    $scope.initialize = function() {
+        $scope.city = "Olathe";
+        $scope.state = "KS";
+        $scope.findWeather( $scope.city, $scope.state );
+    }
 
-    function FindWeather( city, state ) {
-        $( ".weather-entry" ).remove();
-        $( "#which-city" ).html( city + ", " + state );
+    $scope.findWeather = function( city, state ) {
+        var apiStringBase = "http://api.wunderground.com/api/4bbbc25f4f5946dd/hourly/q/"; //state%20code/cityname.json";
         var apiString = apiStringBase + state + "/" + city + ".json";
         console.log( apiString );
 
+        
         $.getJSON( apiString, function( data ) {
-            console.log( data );
-            console.log( data["hourly_forecast"] );
-
+            $scope.weather = [];
+            
             for ( var h = 0; h < 5; h++ )
             {
-                tableStr = "<tr class='weather-entry'>"
-                    + "<td>" + data["hourly_forecast"][h]["FCTTIME"]["hour"] + ":" + data["hourly_forecast"][h]["FCTTIME"]["min"] + "</td>" // time
-                    + "<td>" + data["hourly_forecast"][h]["condition"] + "</td>" // condition
-                    + "<td>" + data["hourly_forecast"][h]["temp"]["english"] + " &#176;F / " + data["hourly_forecast"][h]["temp"]["metric"] + " &#176;C</td>" // temperature
-                    + "<td>" + data["hourly_forecast"][h]["feelslike"]["english"] + " &#176;F / " + data["hourly_forecast"][h]["temp"]["metric"] + " &#176;C</td>" // feels like
-                    + "</tr>";
-                $( "#weather-table" ).append( tableStr );
+                console.log( data["hourly_forecast"][h]["FCTTIME"]["hour"] + ":" + data["hourly_forecast"][h]["FCTTIME"]["min"] );
+                $scope.weather.push( {
+                        time:           data["hourly_forecast"][h]["FCTTIME"]["hour"] + ":" + data["hourly_forecast"][h]["FCTTIME"]["min"],
+                        condition:      data["hourly_forecast"][h]["condition"],
+                        temperature:    data["hourly_forecast"][h]["temp"]["english"] + " F / " + data["hourly_forecast"][h]["temp"]["metric"] + " C",
+                        feelslike:      data["hourly_forecast"][h]["feelslike"]["english"] + " F / " + data["hourly_forecast"][h]["temp"]["metric"] + " C"
+                    } );
             }
         } );
     }
 
-    FindWeather( "Olathe", "KS" );
+    $scope.getWeather = function() {
+        $scope.findWeather( $scope.city, $scope.state );
+    }
 } );
+
+
+
